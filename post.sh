09f90cf1b1c2c6041c6f59e9fa719f019b77f84d
Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>
2024-10-22 15:52:11 -0700

    post.sh: add more commands for testing
    Signed-off-by: Jiaxiao Zhou (Mossaka) <duibao55328@gmail.com>

--- post.sh
+++ post.sh
@@ -1,11 +1,18 @@
 curl --request POST \
      --url http://localhost:3000/v1.0/state/statestore/ \
      --header "Content-Type: application/json" \
-     --data '{
+     --data '[{
        "key": "order",
        "value": {
          "data": {
            "orderId": "42"
          }
        }
-     }'
+     }]'
+
+curl --request POST \
+     --url http://localhost:8080/neworder \
+     --header "Content-Type: application/json" \
+     --data '{"data":{"orderId":"42"}}'
+
+hey -m POST -H "Content-Type: application/json" -d '{"data":{"orderId":"42"}}' -n 1000 -c 10 http://localhost:8080/neworder
